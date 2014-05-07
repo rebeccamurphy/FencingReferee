77f319f2f1c0e0844b0cc837e5ad60751eafad10
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-05-07 00:17:02 -0400

    got title activity up

--- gen/fencing/referee/R.java
+++ gen/fencing/referee/R.java
@@ -22,12 +22,14 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int black_overlay=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int fencinglogo=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
-        public static final int fullscreen_content=0x7f070000;
-        public static final int fullscreen_content_controls=0x7f070001;
+        public static final int epee_btn=0x7f070002;
+        public static final int foil_btn=0x7f070001;
+        public static final int imageView1=0x7f070000;
+        public static final int saber_btn=0x7f070003;
     }
     public static final class layout {
         public static final int activity_title=0x7f030000;
@@ -36,6 +38,9 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int app_name=0x7f050000;
         public static final int dummy_button=0x7f050001;
         public static final int dummy_content=0x7f050002;
+        public static final int epee_str=0x7f050004;
+        public static final int foil_str=0x7f050003;
+        public static final int saber_str=0x7f050005;
     }
     public static final class style {
         /** 
